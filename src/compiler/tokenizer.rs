d75186e1a4658b1196d36f64c38183198c185e78
Txythread <txyzthread@icloud.com>
2025-10-20 20:59:07 +0200

    Add tokenize (classify) functionality skeleton

---
+++ src/compiler/tokenizer.rs
@@ -0,0 +1,9 @@
+use crate::compiler::line_map::LineMap;
+
+
+/// Turn the split string into tokens ("classify" them)
+///
+/// A simple `"10"` would be turned into ImmediateValue(Signed32BitNumber(10)) or whatever the default number system is set to.
+pub fn tokenize(separated: Vec<Vec<String>>, line_map: &mut LineMap) {
+
+}
