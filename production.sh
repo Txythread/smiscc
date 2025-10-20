30e4c7f869eeb133166cca4dc4dc6f89640eb5d5
Txythread <txyzthread@icloud.com>
2025-10-20 16:04:43 +0200

    Added Production Script

---
+++ production.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+cargo build --release 1>/dev/null 2>/dev/null
+
+if [ $? -ne 0 ]; then
+	echo "smiscc ./production.sh: failed to compile smiscc" 1>&2
+fi
+
+
+sudo mv target/release/smiscc /usr/local/bin
