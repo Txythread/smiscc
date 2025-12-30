0340c1479a52561b286b730a3e9c4363a6632c3c
Txythread <txyzthread@icloud.com>
2025-12-30 15:00:51 +0100

    Added Automatic Assembling & Linking (MacOS only)

--- src/compiler/compiler.rs
+++ src/compiler/compiler.rs
@@ -1,3 +1,5 @@
+use clap::{Arg};
+use std::process::Command;
 use crate::compiler::backend::assembly;
 use crate::compiler::backend::context::Context;
 use crate::compiler::backend::flattener::flatten;
@@ -20,6 +22,44 @@ pub fn compile(code: String) {
     if context.line_map.error_count == 0 {
         assembly::generate_assembly(assembly, arch, "test.s".to_string())
     }
+
+    cc::Build::new()
+        .file("test.s")
+        .out_dir("./build/")
+        .target("aarch64-apple-darwin")
+        .opt_level(0)
+        .host("aarch64-apple-darwin")
+        .compile("test");
+
+    let xc_command = Command::new("xcrun")
+        .args(["-sdk", "macosx", "--show-sdk-path"])
+        .output()
+        .unwrap();
+
+    let syslibroot = String::from_utf8(xc_command.stdout).unwrap();
+
+    println!("syslibroot: {}", syslibroot);
+
+    let mut command = Command::new("ld");
+
+    for entry in glob::glob("./build/*-test.o").unwrap() {
+        command.arg(entry.unwrap().to_string_lossy().to_string());
+    }
+
+    command
+        .arg("-o")
+        .arg("test")
+        .arg("-lSystem")
+        .arg("-syslibroot")
+        .arg(syslibroot.trim())
+        .arg("-e")
+        .arg("_start")
+        .arg("-arch")
+        .arg("arm64");
+
+    println!("output: {}", String::from_utf8_lossy(&command.output().unwrap().stdout));;
+
+
 
     context.line_map.display_finish();
 }
